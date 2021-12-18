bd9084c268e275c1c333c203b54f04ed3c4942a3
Shafin <shafinbinshakeel@gmail.com>
2021-12-18 23:10:55 +0530

    full

---
+++ addfullstopatend.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()									//program to add full stop in the end if string 
+{
+	char s[100],t[]=".";
+	printf("enter the string\n");
+	gets(s);
+	int l,i;
+	l=strlen(s);
+	strcat(s,t);
+	puts(s);
+}
